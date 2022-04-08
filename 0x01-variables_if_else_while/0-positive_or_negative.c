3cf6c1fb89da30b755c558abbde8d11f7fecef60
eshetuD <eshetu.deresu3@gmail.com>
2022-04-08 19:40:12 +0000

    My commit for task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/Correct)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n==0)
+	{
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+My read me file for task 0x01-variables_if_else_while
