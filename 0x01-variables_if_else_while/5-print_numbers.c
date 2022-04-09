04f522fdadbd43a1813412f448126c3b12306b5a
eshetuD <eshetu.deresu3@gmail.com>
2022-04-09 06:25:42 +0000

    my commit for print numbers task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print numbers 0 to 9 on one line
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+ptinf("%i", i);
+
+return (0);
+}
