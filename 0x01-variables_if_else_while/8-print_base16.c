57b04161f2e9fc6f339f657eb2fa0cced39f8c81
eshetuD <eshetu.deresu3@gmail.com>
2022-04-09 06:50:23 +0000

    my commitfor task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int x;
+
+for (x = 0; x <= 9; x++)
+printf("%i", x);
+for (x = 'a'; x <= 'f'; ++x)
+putchar(x);
+
+putchar('<n');
+
+return (0);
+}
