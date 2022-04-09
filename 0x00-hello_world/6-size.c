55be5c568715b394251f44580a7774f760bf5e32
eshetuD <eshetu.deresu3@gmail.com>
2022-04-09 08:03:20 +0000

    my commit for task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+
+printf("Size of a char: %lu byte(s)<n", (unsigned long)sizeof(d));
+
+return (0);
+}
