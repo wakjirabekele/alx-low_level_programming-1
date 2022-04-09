c02746da677c62b0d00bc108d073f1601a50559a
eshetuD <101511990+eshetuD@users.noreply.github.com>
2022-04-09 11:20:11 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 #include <unistd.h>
 /**
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-write(1, "and that piece of art is useful<" - Dora Korpar, 2015-10-19<n", 59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return(1);
 }
