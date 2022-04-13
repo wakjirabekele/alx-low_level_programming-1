47ede189a3940abbff0e652530338759b75b622c
eshetuD <eshetu.deresu3@gmail.com>
2022-04-13 05:32:23 +0000

    my commit for task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,7 @@
 /**
  * _islower - returns 1 if lowercase alphabet and 0 otherwise
  * @C: takes in a character
+ * c: parameter for the method
  * Return: 0 for uppercase, 1 for lowercase
  */
 int _islower(int c)
