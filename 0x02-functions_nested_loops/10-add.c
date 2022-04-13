d64bf807321bf9ce305d31093ec168474607109c
eshetuD <eshetu.deresu3@gmail.com>
2022-04-13 07:04:49 +0000

    my commit for task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/** add - function that add two integer
+/** 
+ * add - function that add two integer
  * @a: takes value of a
  * @b: takes the value of b
  * Return: return the sum of two integer
