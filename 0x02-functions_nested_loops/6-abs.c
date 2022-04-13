4c944cb8a340f7688298e1a7002169d36030ace0
eshetuD <eshetu.deresu3@gmail.com>
2022-04-13 06:15:26 +0000

    my commit for task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * abs - function  that print the absolute value of a number
+ * _abs - function  that print the absolute value of a number
  * @n: takes in an integer
  * Return: 1 if >0, 0 if == 0, positive if negative
  */
