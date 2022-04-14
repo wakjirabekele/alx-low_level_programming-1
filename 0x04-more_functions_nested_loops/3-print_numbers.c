8fe6beeca0df03ee1c01ae3c620c5901ec616c8d
eshetuD <eshetu.deresu3@gmail.com>
2022-04-14 11:30:48 +0000

    my commit for task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print the numbers fro 0 to 9
+ * Return: always 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
