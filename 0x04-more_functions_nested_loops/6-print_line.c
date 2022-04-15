54fdfc6d97026ba2128bcc8892ca8683b343b555
eshetuD <eshetu.deresu3@gmail.com>
2022-04-15 06:57:02 +0000

    my commit for task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - print straight line
+ * @n: input value
+ * Return: always 0
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar (95);
+		}
+
+		_putchar ('\n);
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+}
