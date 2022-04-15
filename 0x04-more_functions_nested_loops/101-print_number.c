ae415ca0b179445e8bb278e098da2af893d56dbc
eshetuD <eshetu.deresu3@gmail.com>
2022-04-15 10:53:30 +0000

    my commit task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,8 +3,10 @@
 /**
  * print_number - print function
  * @n: input variable
+ *
  * Return: always 0
  */
+
 void print_number(int n)
 {
 	unsigned int m;
@@ -19,7 +21,6 @@ void print_number(int n)
 		m = n;
 	}
 	if (m / 10 != 0)
-		print_number (m / 10);
-
+	print_number (m / 10);
 	_putchar((m % 10) + '0');
 }
