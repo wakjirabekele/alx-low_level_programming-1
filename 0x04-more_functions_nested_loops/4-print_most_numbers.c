6344032db0df6b078fc7bda96278f917ef1065a1
eshetuD <eshetu.deresu3@gmail.com>
2022-04-15 06:10:06 +0000

    my commit for task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
  * print_most_numbers - print except 2 and 4
+ *
  * Return: always 0
  */
 void print_most_numbers(void)
 {
+
 	int i;
 
 	i = 0;
-
 	for (i = 0; i < 10; i++)
 	{
 
