df7b9088bafb7110216ff5a3c12b94070bf6c5ea
eshetuD <eshetu.deresu3@gmail.com>
2022-04-15 07:46:40 +0000

    my commit for task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 3 != 0 && i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+			putchar(' ');
+		else
+			putchar('\n');
+	}
+	return (0);
+}
