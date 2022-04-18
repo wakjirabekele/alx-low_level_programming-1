e0b1ecf1f96db75800746f70dfd2d77333d941aa
eshetuD <eshetu.deresu3@gmail.com>
2022-04-18 08:55:19 +0000

    my commit for task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - length of a string
+ * @s: a pointer parameter
+ * Return: void method
+ */
+int _strlen(char *s)
+{
+	int u;
+
+	u = 0;
+	whileb(s[u] != '\0')
+	{
+		u++;
+	}
+	return (u);
+}
