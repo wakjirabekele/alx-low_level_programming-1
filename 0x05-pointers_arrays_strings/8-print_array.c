faba6edc5f02c07fb78fa56175b66592c05f17d2
eshetuD <101511990+eshetuD@users.noreply.github.com>
2022-04-18 22:43:57 +0300

    my commit for task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include 'main.h"
+#include <stdio.h>
+/**
+* print_array - function print array
+* @a: parameter 1
+* @n: size of the array
+* Return: void method
+*/
+void print_array(int *a, int n)
+{
+int i;
+
+i = 0;
+while (i < n)
+{
+printf("%d", a[i]);
+
+if (i < n - 1)
+{
+printf(", ");
+}
+
+i++;
+}
+
+printf("\n");
+}
