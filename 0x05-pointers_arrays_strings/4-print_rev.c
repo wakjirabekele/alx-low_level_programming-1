270a02ed1f1c92a0445e82bfb941cf532747c91a
eshetuD <eshetu.deresu3@gmail.com>
2022-04-18 09:13:41 +0000

    my commit for task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - print reverse order
+ * @s: a pointer parameter
+ * Return: void method
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for ( i = i - 1; i >= 0; i--)
+	{
+		_putchar (s[i]);
+	}
+
+	_putchar ('\n');
+}
