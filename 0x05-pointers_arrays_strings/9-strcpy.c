4ce0c61e7e0888cea7a7d0f7d772007fc2348d4a
eshetuD <101511990+eshetuD@users.noreply.github.com>
2022-04-18 22:51:23 +0300

    my commit for task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _strcpy - function to copy string
+* @dest: destination of copied string
+* @src: source of the string
+* Return: destination
+*/
+char *_strcpy(char *dest, char *src)
+{
+int i;
+
+i = 0;
+
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\n';
+
+return (dest);
+}
