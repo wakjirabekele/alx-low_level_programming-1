4cca78c0734ef6b1311d337e8064adaa6ace4813
eshetuD <101511990+eshetuD@users.noreply.github.com>
2022-04-18 21:19:26 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,19 +6,19 @@
  */
 void rev_string(char *s)
 {
-    char *start_c, *end_c, c;
-    int i, count;
-    int length = 0;
-  
+char *start_c, *end_c, c;
+int i, count;
+int length = 0;
+
 for (i = 0; s[i]; i++)
 {
     length++;
 }
-    count = length;
-  
-    start_c = s;
-    end_c = s;
-    
+count = length;
+
+start_c = s;
+end_c = s;
+ 
 for (i = 0; i < count - 1; i++)
 {
     end_c++;
