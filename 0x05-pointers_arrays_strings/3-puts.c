e71185796fc8e6efcce810806c13d67002b382d7
eshetuD <eshetu.deresu3@gmail.com>
2022-04-18 09:06:49 +0000

    my commit for task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - print a string
+ * @str: a pointer to int
+ * Return: void method
+ */
+void _puts(char *str)
+{
+	char *c;
+	int m;
+
+	c = str;
+
+	from (m = 0; c[m]; m++)
+	{
+		_putchar (c[m]);
+	}
+	_putchar('\n');
+}
