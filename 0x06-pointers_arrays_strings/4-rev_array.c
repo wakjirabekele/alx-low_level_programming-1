0df1f8f6bfadaeeb9934933eba780741a238ba0a
eshetuD <eshetu.deresu3@gmail.com>
2022-04-20 12:36:29 +0000

    my commit for task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * reverse_array - reverse the given array
+ * @a: an array of integer
+ * @n: number of element in the array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int *start_c, *end_c, c;
+	int i;
+
+	start_c = a;
+	end_c = a;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		end_c++;
+	}
+
+	for (i = 0; i < n / 2; i++)
+	{
+		c = *end_c;
+		*end_c = *start_c;
+		*start_c = c;
+
+		start_c++;
+		end_c--;
+	}
+}
