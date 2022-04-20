035cabc98e731fb0561e6e4eee6da146ea458a32
eshetuD <eshetu.deresu3@gmail.com>
2022-04-20 12:29:05 +0000

    my commit for task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - compare two string
+ * @s1: the first string
+ * @s2: the second string
+ * Return: comparison
+ */
+int _strcmp(char *s1, char *s2)
+{
+	char *str_one = s1;
+	char *str_two = s2;
+
+	while(*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
+	{
+		str_one++;
+		str_two++;
+	}
+
+	return (*str_one - *str_two);
+}
