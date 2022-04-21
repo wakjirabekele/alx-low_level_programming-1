872562b2f2e909c52a0f843b87589943f2278602
eshetuD <eshetu.deresu3@gmail.com>
2022-04-21 06:38:56 +0000

    my commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,18 @@ int _strcmp(char *s1, char *s2)
 	char *str_one = s1;
 	char *str_two = s2;
 
-	while(*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
+	while(*str_one != '\0' && *str_two != '\0')
 	{
-		str_one++;
-		str_two++;
+		if (*str_one == *str_two)
+		{
+			str_one++;
+			str_two++;
+		}
+		else
+		{
+			break;
+		}
 	}
 
-	return (*s1 - *s2);
+	return (*str_one - *str_two);
 }
