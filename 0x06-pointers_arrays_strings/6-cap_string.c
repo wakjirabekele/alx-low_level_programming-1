1977bc6d05f8e1e272abaa95bf6516b7e61502d0
eshetuD <eshetu.deresu3@gmail.com>
2022-04-21 16:44:18 +0000

    my commit for task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * cap_string - capitalize each word
+ * @str: the parameter
+ * Return: character[B
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+		else if (str[i-1] == ' ')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+	}
+	return (str);
+}
