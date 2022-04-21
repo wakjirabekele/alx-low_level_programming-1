c3f690d141fb30398bbea8f466df6f6d714725cb
eshetuD <eshetu.deresu3@gmail.com>
2022-04-21 05:55:47 +0000

    my commit for task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,13 +12,14 @@ char *_strncat(char *dest, char *src, int n)
 
 	i = 0;
 	while (dest[i] != '\0')
-{
-	i++;
+	{
+		i++;
 	}
 
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
+		dest[i] = src[j];
 		j++;
 		i++;
 	}
