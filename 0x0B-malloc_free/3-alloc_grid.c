2831b1e3382870570d4afa45ba5ce0ceee34ab0e
eshetuD <101511990+eshetuD@users.noreply.github.com>
2022-05-02 16:23:33 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * alloc_grid - allocates a grid, make space and free space
- * @width: takes in width of grid
- * @height: height of grid
- * Return: grid with freed spaces
+* alloc_grid - allocates a grid, make space and free space
+* @width: takes in width of grid
+* @height: height of grid
+* Return: grid with freed spaces
  */
 
 int **alloc_grid(int width, int height)
