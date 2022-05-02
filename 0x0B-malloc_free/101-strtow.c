fb77826145235e9cbca39ff6ceb62bdc9a2acd63
eshetuD <101511990+eshetuD@users.noreply.github.com>
2022-05-02 16:45:48 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,46 +1,7 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-/**
- * strtow - splits a string into words
- * @str: string of words to be split
- * Return: double pointer to strings
- */
-char **strtow(char *str)
-{
-	char **ptr;
-	int i, k, len, start, end, j = 0;
-	int words =  countWords(str);
 
-	if (!str || !countWords(str))
-		return (NULL);
-	ptr = malloc(sizeof(char *) * (words + 1));
-	if (!ptr)
-		return (NULL);
-	for (i = 0; i < words; i++)
-	{
-		start = startIndex(str, j);
-		end = endIndex(str, start);
-		len = end - start;
-		ptr[i] = malloc(sizeof(char) * (len + 1));
-		if (!ptr[i])
-		{
-			i -= 1;
-			while (i >= 0)
-			{
-				free(ptr[i]);
-					i--;
-			}
-			free(ptr);
-			return (NULL);
-		}
-		for (k = 0; k < len; k++)
-			ptr[i][k] = str[start++];
-		ptr[i][k++] = '\0';
-		j = end + 1;
-	}
-	ptr[i] = NULL;
-	return (ptr);
-}
 
 /**
  * isSpace - determines if character is a space or not
@@ -101,4 +62,46 @@ int countWords(char *s)
 		s++;
 	}
 	return (words);
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: string of words to be split
+ * Return: double pointer to strings
+ */
+char **strtow(char *str)
+{
+	char **ptr;
+	int i, k, len, start, end, j = 0;
+	int words =  countWords(str);
+
+	if (!str || !countWords(str))
+		return (NULL);
+	ptr = malloc(sizeof(char *) * (words + 1));
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i < words; i++)
+	{
+		start = startIndex(str, j);
+		end = endIndex(str, start);
+		len = end - start;
+		ptr[i] = malloc(sizeof(char) * (len + 1));
+		if (!ptr[i])
+		{
+			i -= 1;
+			while (i >= 0)
+			{
+				free(ptr[i]);
+					i--;
+			}
+			free(ptr);
+			return (NULL);
+		}
+		for (k = 0; k < len; k++)
+			ptr[i][k] = str[start++];
+		ptr[i][k++] = '\0';
+		j = end + 1;
+	}
+	ptr[i] = NULL;
+	return (ptr);
 }
