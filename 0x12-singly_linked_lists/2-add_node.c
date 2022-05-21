2fe93fddb35ce86c73e02456d505324567404edc
eshetuD <101511990+eshetuD@users.noreply.github.com>
2022-05-21 03:36:29 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,29 +1,88 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
- * add_node - adds a new node at the beginning
- * of a list_t list.
- * @head: head of the linked list.
- * @str: string to store in the list.
- * Return: address of the head.
+ * add_node - function that adds a new node at the beginning of a list_t list
+ * @head: pointer to the pointer to the struct named head
+ * @str: pointer to the string str
+ *
+ * Return: pointer to the struct list_t
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new;
-    size_t nchar;
+	list_t *temp;
 
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-        return (NULL);
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+	{
+		free(temp);
+		return (NULL);
+	}
+	temp->str = _strdup(str);
+	temp->len = _strlen(str);
+	temp->next = *head;
+	*head = temp;
+	return (*head);
+}
 
-    new->str = strdup(str);
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: parameter defined in main
+ *
+ * Return: length of string
+ */
 
-    for (nchar = 0; str[nchar]; nchar++)
-        ;
+int _strlen(const char *s)
+{
+	int i = 0;
 
-    new->len = nchar;
-    new->next = *head;
-    *head = new;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
 
-    return (*head);
+/**
+ * _strdup - function that returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string given as a parameter
+ *
+ * @str: string of chars
+ *
+ * Return: address of the newly allocated memory
+ */
+
+char *_strdup(const char *str)
+{
+	unsigned int len;
+	unsigned int i, j;
+	char *str_copy;
+	const char *tmp = str;
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (*str++)
+		i++;
+	len = i;
+	str = tmp;
+
+	str_copy = malloc(len * sizeof(char) + 1);
+	if (str_copy == NULL)
+		return (NULL);
+
+	j = 0;
+	while (j < len)
+	{
+		str_copy[j] = str[j];
+		j++;
+	}
+	str_copy[j] = '\0';
+	return (str_copy);
 }
