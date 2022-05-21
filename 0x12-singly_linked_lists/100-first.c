d983120bcc95e4dd37ce2b2116821f6ad6b97ee3
eshetuD <101511990+eshetuD@users.noreply.github.com>
2022-05-21 03:39:56 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,16 @@
+#include "lists.h"
 #include <stdio.h>
 
-/**
- * bmain - function executed before main
- * Return: no return.
- */
+void print_before_main(void) __attribute__ ((constructor));
 
-void __attribute__((constructor)) bmain()
+/**
+ * print_before_main - function that prints before the main
+ * fonction is executed
+ *
+ * Return: void
+ */
+void print_before_main(void)
 {
-    printf("You're beat! and yet, you must allow");
-    printf(",\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
 }
